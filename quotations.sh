7abc723ebdf95aa5ec5a1291b5a949b38235c3ea
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-13 10:46:25 -0500

    Add printf.sh

--- quotations.sh
+++ quotations.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#Strings are represented by quotations. In bash, there are different functions for each type of quotations.
 #This script demonstrates how to properly use quotations in Bash.
 name='Zack' #this is variable name with value "Zack."
-echo "Hi $name" #Using double quotations when echoing allows for calling variables as done here, meaning the output will appear as "Hi Zack"
-echo 'Hi $name' #However, using single quotations when echoing will interpret the value ONLY as string, meaning the output will appear as 'Hi $name'
+echo "Hi $name (using double quotes)" #Using double quotations when echoing allows for calling variables as done here, meaning the output will appear as "Hi Zack"
+echo 'Hi $name (using single quotes)' #However, using single quotations when echoing will interpret the value ONLY as string, meaning the output will appear as 'Hi $name'.
+#When echoing strings which may contain a variable, use double quotes, but when echoing string without any variables, use single quotes.
+#This could be important to keep in mind when creating a program, as proper implementation of quotations could potentially prevent injection attacks where possible.
--- variables.sh
+++ variables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Variables are partitions of memory in a computer program which store data; something which stores something.
 #This script demonstrates the implementation of variables in Bash.
-name="Zack" #This is how a variable is set. the variable "name" is declared and set with a string value "John".
+name='Zack' #This is how a variable is set. the variable "name" is declared and set with a string value "John".
 echo "Hi $name" #To echo a variable, you must call it by prepending a dollar sign ($) to the variable name as done here.
