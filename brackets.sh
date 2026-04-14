30a3b8bd362cd4f08473842a5b7fccc124f414c3
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-14 10:48:27 -0500

    add code and fixes to brackets.sh

--- brackets.sh
+++ brackets.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #In bash, different formats of brackets are used for different purposes. Below is code which demonstrates an example of said formats and their functions.
 directory='directory_name'
-(directory=cheese; mkdir $a)
+(directory=cheese; mkdir "$directory")
 #The code above uses single brackets. Single brackets are used to run commands in what is called a subshell.
 #Subshells are isolated child instances of a shell, which can be spawned by utilizing these single brackets.
 arithmetic=$((76 + 45))
 echo "$arithmetic"
 #The code above uses double dollar brackets, which are used to perform and return arithmetic as demonstrated here.
 #Double dollar brackets are used here to calculate the sum of 76 and 45 and store it to variable 'arithmetic', which is then echoed to the terminal output.
+if [ 1 =  0 ]; then
+	echo "Statement is true"
+else
+	echo "Statement is false"
+fi
