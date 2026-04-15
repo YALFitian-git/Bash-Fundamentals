0687310a016935943750d16f331f5165c75335e3
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-15 10:49:25 -0500

    Add initial comments and code to conditionals.sh

---
+++ conditionals.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Conditionals in programming are functions which determine the trueness of a statement and what to do if said statement is/isn't.
+#This bash script demonstrates the implementation of conditionals in Bash.
+selected_number=24
+if [selected_number  < 1 ]; then
+	echo "This number is less than 1"
+fi
