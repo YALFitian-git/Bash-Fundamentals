4bde702e22614ad64fbb0bffa2e0a82cd873fd0a
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-17 10:24:09 -0500

    Add more code to input.sh

--- input.sh
+++ input.sh
@@ -5,5 +5,8 @@ username='user'
 read -p "Enter your username: " username_attempt
 password='Password123'
 read -s -p "Enter your password: " password_attempt
+if [ $username_attempt = $username ] && [ $password_attempt = $password ]; then
+	echo "Login successful, welcome $username"
+fi
 #Above is a basic replica of a login portal. The "read" command is responsible for recieving user input.
 #The -p flag allows for displaying a prompt before listening for input, and the -s flag hides the typed input. This is commonly used for sensitive data such as passwords as demonstrated here.
