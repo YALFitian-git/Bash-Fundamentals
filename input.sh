f45fbf33e5483ad3b8077cbb5d47525ac975639d
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-17 10:44:45 -0500

    Fix code in input.sh

--- input.sh
+++ input.sh
@@ -2,16 +2,15 @@
 #Programming languages possess the capability to recieve input from the user running the program
 #Such a concept is present in Bash. Below is an example of recieving user input and using it in programming.
 username='user'
-read -p "Enter your username: " username_attempt
 password='Password123'
+read -p "Enter your username: " username_attempt
 read -s -p "Enter your password: " password_attempt
-if [ $username_attempt = $username ] && [ $password_attempt = $password ]; then #This 'if' statement checks two conditionals with "&&".
+if [ $username_attempt == $username ] && [ $password_attempt == $password ]; then #This 'if' statement checks two conditionals with "&&".
 	echo "Login successful, welcome $username" #If both are satisfied, login is successful.
+
 else
 	clear #Clear the screen
-	echo 'Incorrect username or password, please try again' #If one of the statements or both are not satisfied, login is unsuccessful and asks for another input.
-	read -p "Enter your username: " username_attempt
-	read -s -p "Enter your password: " password_attempt
+	echo 'Incorrect username or password' #If one of the statements or both are not satisfied, login is unsuccessful and asks for another input.
 fi
 #Above is a basic replica of a login portal. The "read" command is responsible for recieving user input.
 #The -p flag allows for displaying a prompt before listening for input, and the -s flag hides the typed input. This is commonly used for sensitive data such as passwords as demonstrated here.
