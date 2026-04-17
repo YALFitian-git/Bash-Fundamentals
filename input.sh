83bbf5566a0393633b444a9d22a4a6585da650f1
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-17 10:16:05 -0500

    Update code and add comments to input.sh

--- input.sh
+++ input.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-read -p "Enter your name: " name
-echo "Hi $name"
+#Programming languages possess the capability to recieve input from the user running the program
+#Such a concept is present in Bash. Below is an example of recieving user input and using it in programming.
+username='user'
+read -p "Enter your username: " username_attempt
+password='Password123'
+read -s -p "Enter your password: " password_attempt
+#Above is a basic replica of a login portal. The "read" command is responsible for recieving user input.
+#The -p flag allows for displaying a prompt before listening for input, and the -s flag hides the typed input. This is commonly used for sensitive data such as passwords as demonstrated here.
