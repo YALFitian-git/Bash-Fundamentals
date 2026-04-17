ef25a9c0551544b968fd8212acd9983dd04b2712
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-17 10:47:59 -0500

    Add "clear" command to satisfied if statement in input.sh

--- input.sh
+++ input.sh
@@ -6,10 +6,11 @@ password='Password123'
 read -p "Enter your username: " username_attempt
 read -s -p "Enter your password: " password_attempt
 if [ $username_attempt == $username ] && [ $password_attempt == $password ]; then #This 'if' statement checks two conditionals with "&&".
+	clear #Clear the screen
 	echo "Login successful, welcome $username" #If both are satisfied, login is successful.
 
 else
-	clear #Clear the screen
+	clear 
 	echo 'Incorrect username or password' #If one of the statements or both are not satisfied, login is unsuccessful and asks for another input.
 fi
 #Above is a basic replica of a login portal. The "read" command is responsible for recieving user input.
