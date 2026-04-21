d24f1cb774234f05b39ca12d78b11eaa3ba43ae0
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-21 10:42:44 -0500

    Add comments to conditionalflags.sh

--- conditionalflags.sh
+++ conditionalflags.sh
@@ -8,10 +8,13 @@ else
 fi
 #The example code above uses -n, a string-based flag. -n is used to check if a variable is not empty, and returns true if such is the case.
 #Alternatively, one could use -z, which returns true if the string is empty.
-read -p 'Enter your first input value: ' input1
-read -p 'Enter your second input value: ' input2
+read -p 'Enter your first input value (1 or 0): ' input1
+read -p 'Enter your second input value (1 or 0): ' input2
 if [ $input1 -eq 1 ] && [ $input2 -eq 1 ]; then
 	echo '1 and 1 in an AND gate give an output of 1'
 else
 	echo "$input1 and $input2 in an AND gate give an output of 0"
 fi
+#The code above is a demonstration of an AND gate.
+#The code uses -eq  flags, which are the letter-based versions of the equality expression. They return true if 2 values equal to each other.
+#&& is also used, which allows for testing two or more statements at a time. However, the if statement will only return true if ALL statements return true.
