a2e032bab5f220e370dd9369631580b1f231d90f
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-29 10:46:13 -0500

    Add code and comments to arrays.sh

---
+++ arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Arrays in programming can be defined as a list of values represented by a single variable.
+fruits=("Apples" "Oranges" "Bananas" "Grapes" "Kiwis")
+echo "My favourite fruits are ${fruits[1]}"
+#Above is a basic example of an array and an echo that returns one of its values.
+#The array has 5 items. Changing the number in ${fruits[]} from 0 - 4 (The number of an array item starts at 0) will change the output as it gets a different item.
