03513a22a4f1a9f8fe7335dd8d66d8ce4eefe1ae
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-29 10:14:45 -0500

    Add code to casestatements.sh

--- casestatements.sh
+++ casestatements.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 #Case statements are used in programming to substitute the need to create a large chain of if-elif-else statements.
+read -p "Select an option: (1,2,3)" choice
+case $choice in
+	1)
+		option='option 1'
+		;;
+	2)
+		option='option 2'
+		;;
+	3)
+		option='option 3'
+		;;
+	*)
+		echo 'Invalid input'
+		exit
+esac
+echo "You have selected $option"
