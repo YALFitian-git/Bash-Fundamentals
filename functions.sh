8f49f1916794119e4512462e1936a77d24916f0b
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-04-30 10:34:32 -0500

    Modify/fix code and more comments in functions.sh

--- functions.sh
+++ functions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Functions in programming are self-contained groups of multiple lines of code. They are often used when a set of lines of code is needed or expected to be used more than once.
-val_1=8
-val_2=5
+read -p 'Input your first value: ' val_1
+read -p 'Input your second value: ' val_2
 function add {
-	echo "(($val_1 + $val_2))"
+	echo "$((val_1 + val_2))"
 }
 function subtract {
-	echo "(($val_1 - $val_2))"
+	echo "$((val_1 - val_2))"
 }
 read -p "Choose to add or subtract: (+ or -)" operator
 case $operator in
@@ -18,3 +18,7 @@ case $operator in
 	;;
 *)
 	echo 'Invalid Input'
+esac
+#The code above is an example of a basic add/subtract calculator that uses functions as operators.
+#The code inside the function uses dollar double brackets to return the arithmetic result of var_1 and var_2.
+#Functions are often used in programming to avoid having to reuse multiple lines of code and instead call upon something which is easier to type and still retains the target function of said code.
