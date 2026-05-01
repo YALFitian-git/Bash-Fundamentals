e1dea0c51ed08a1169bfe0c4e745d614dc7cf3a5
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-05-01 10:31:52 -0500

    Add more code and comments to defaultvalues.sh

--- defaultvalues.sh
+++ defaultvalues.sh
@@ -3,4 +3,7 @@
 #In bash, there are many ways to implement them.
 read -p 'Select a port to listen on: ' port
 echo "Listening on port ${port:=8000}"
-#This is a basic model of a network port listener prompt. In this example, the prompt sets the variable port to 8000 if a user input is unspecified.
+#This is a basic model of a network port listener prompt. In this example, the prompt uses ${var:=default} to set the variable port to 8000 if a user input is unspecified.
+read -p 'What is your name? ' name
+echo "Hello ${name:='user'}"
+#In this example, the prompt uses ${var:-default} to echo 'user' if a name is not specified. Note that it does not set the variable 'name' to 'user'.
