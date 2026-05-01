bf001bcfad6d43d122296e61f83642a03c9a2f06
yalfitian <yalfitian@debian13.myguest.virtualbox.org>
2026-05-01 10:28:00 -0500

    Add comments to defaultvalues.sh

--- defaultvalues.sh
+++ defaultvalues.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+#Default values are values which a program may fall back to if a variable is not set or empty.
+#In bash, there are many ways to implement them.
 read -p 'Select a port to listen on: ' port
-echo "Listening on port ${port:-8000}"
+echo "Listening on port ${port:=8000}"
+#This is a basic model of a network port listener prompt. In this example, the prompt sets the variable port to 8000 if a user input is unspecified.
