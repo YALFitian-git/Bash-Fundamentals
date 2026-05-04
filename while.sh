0c04ef8dabf86b74da62778a62c3c5d8405361e6
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-04 10:48:31 -0500

    Add comments to while.sh

--- while.sh
+++ while.sh
@@ -5,3 +5,6 @@ while [[ $input -lt 10 ]]
 do
 read -p 'This number is smaller than 10. Try again: ' input
 done
+#The code above is an example of a while loop. It uses conditional statements the syntax of which is identical to that in if statements.
+#If you run the code, the while loops will continuously ask for input until said input exceeds or equals to the number 10.
+#While loops can be used in such a manner in areas such as login portals, captchas, and so on.
