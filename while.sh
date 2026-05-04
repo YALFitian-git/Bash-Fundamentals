c3922bb50ba6931ea8e66e73b15e6d39ae1571a0
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-04 10:44:04 -0500

    Change code of while.sh

--- while.sh
+++ while.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #While loops are loops in programming that continue to execute code within themelves for as long as a checked statement remains satisfied.
-read -p 'Enter a username: ' input
-while $[[ input -z ]]
+read -p 'Enter a number larger than or equal to 10:  ' input
+while [[ $input -lt 10 ]]
 do
-read -p 'Field is blank. Enter A username: ' input
+read -p 'This number is smaller than 10. Try again: ' input
 done
