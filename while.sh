e2c725b341bd38b04c3cfb8c29126edb3077a5ab
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-04 10:40:26 -0500

    Add code to while.sh

--- while.sh
+++ while.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #While loops are loops in programming that continue to execute code within themelves for as long as a checked statement remains satisfied.
+read -p 'Enter a username: ' input
+while $[[ input -z ]]
+do
+read -p 'Field is blank. Enter A username: ' input
+done
