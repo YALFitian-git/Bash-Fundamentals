8221f751ead6f95b86bdfd4c6f22480014c5a8ab
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-05 10:20:32 -0500

    Add comments to pipes.sh

---
+++ pipes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Pipes in bash are control operators which are used to direct the output of one command into the input of another.
+#Pipes operate in one direction, moving from the left to the right.
