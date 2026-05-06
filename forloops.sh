755267055e9246bf24fa9b50508f898f3d20ba7a
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-06 10:47:46 -0500

    Add code to forloops.sh

--- forloops.sh
+++ forloops.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #For loops in programming are loops of code which repeat for a specific number of iterations.
+echo 'This is your shopping list: '
+list=('Tomatoes' 'Ketchup'  'Salt'  'Pepper'  'Mayonnaise')
+for item in ${list[@]}; do
+	echo $item
+done
