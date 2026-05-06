f3b4231d9c1f1e245e0cf5049c6d19652dc79727
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-06 10:18:51 -0500

    Add code to functions.sh

--- redirects.sh
+++ redirects.sh
@@ -4,3 +4,5 @@ touch log.txt
 ls > log.txt
 echo 'Command output saved to log.txt'
 #The code above is an example of a command logging system. The output of the command ls is redirected to log.txt via single chevrons, which overwrites the content inside.
+echo 'Output of command ls' >> log.txt
+#The code above uses double-chevron redirection, which appends/adds to the log file instead of overwriting its contents.
