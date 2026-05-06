938fc5d5cdd6b6e33c2f2a8668346968f9043394
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-06 10:15:36 -0500

    Add code to redirects.sh

--- redirects.sh
+++ redirects.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #Redirection in bash allows you to change where a command recieves input and where it sends output.
+touch log.txt
+ls > log.txt
+echo 'Command output saved to log.txt'
+#The code above is an example of a command logging system. The output of the command ls is redirected to log.txt via single chevrons, which overwrites the content inside.
