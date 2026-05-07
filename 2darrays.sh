9a56a2af7d50eb82b223366fcbf2804bd6d74cc7
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-07 10:31:10 -0500

    Add comments to 2darrays.sh

---
+++ 2darrays.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#2-Dimensional arrays in programming are data structures which extend to 2 dimensions, i.e. an array of arrays.
+#They are not natively supported by Bash. However, they can be emulated using something called associative arrays.
