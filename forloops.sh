c3d159348eceab67aeec46c4ec9d4ed4b904866c
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-07 10:14:39 -0500

    Add comments to forloops.sh

--- forloops.sh
+++ forloops.sh
@@ -5,3 +5,5 @@ list=('Tomatoes' 'Ketchup'  'Salt'  'Pepper'  'Mayonnaise')
 for item in ${list[@]}; do
 	echo $item
 done
+#The code above is an example of a for loop. The for loop is used to echo each item in the array.
+#For loops are often used with arrays. the @ symbol represents the number of the item in an array.
