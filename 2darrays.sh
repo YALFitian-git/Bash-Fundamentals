f834284bfc6e75c0255a92ff78fe6ed2aed0d06a
yalfitian <158461535+YALFitian-git@users.noreply.github.com>
2026-05-07 10:41:41 -0500

    Add code and comments to 2darrays.sh and correct coordinate mappings

--- 2darrays.sh
+++ 2darrays.sh
@@ -2,7 +2,10 @@
 #2-Dimensional arrays in programming are data structures which extend to 2 dimensions, i.e. an array of arrays.
 #They are not natively supported by Bash. However, they can be emulated using something called associative arrays.
 declare -A matrix
-matrix[0,0]='Top-Left'
-matrix[0,1]='Top-Right'
-matrix[1,0]='Bottom-Left'
-matrix[1,1]='Bottom-Right'
+matrix[1,0]='Top-Left'
+matrix[1,1]='Top-Right'
+matrix[0,0]='Bottom-Left'
+matrix[0,1]='Bottom-Right'
+echo "${matrix[1,0]}"
+#The code above is a basic interpretation of a 2x2 array using associative arrays.
+#An associative array is declared with 'declare -A', and the coordinated are mapped with matrix[x,y]. The code then echoes the string value of the given position.
